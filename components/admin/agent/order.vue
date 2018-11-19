fe16e3f97d32c5765be18fbce924dc688687534a
pedrojuan <peter.damondamon@davao.sti.edu>
2018-11-19 20:09:36 +0800

    configure components/admin/agent/order

--- components/admin/agent/order.vue
+++ components/admin/agent/order.vue
@@ -1,44 +1,67 @@
 <template>
-  <v-flex xs12 sm9 md9 offset-xs1>
-    <v-card class='card' v-for='(item, index) in items' :key="index">
+  <v-flex
+    xs12
+    sm9
+    md9
+    offset-xs1>
+    <v-card
+      v-for="(item, index) in items"
+      :key="index"
+      class="card">
       <v-list>
         <v-list-group>
-          <v-list-tile slot='activator'>
-            <v-card-title><span>{{item.ordnameto}}</span></v-card-title>
+          <v-list-tile
+            slot="activator">
+            <v-card-title><span>{{ item.ordnameto }}</span></v-card-title>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Order Location: </span> <span class="black--text">{{item.address}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Order Location: </span> <span
+                  class="black--text">{{ item.address }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Client Email: </span> <span class="black--text">{{item.ordsend}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Client Email: </span> <span
+                  class="black--text">{{ item.ordsend }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Confirmed Quotation: </span> <span class="black--text">{{confirmed(item.confirmquotation)}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Confirmed Quotation: </span> <span
+                  class="black--text">{{ confirmed( item.confirmquotation ) }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Confirmed Payment: </span> <span class="black--text">{{confirmed(item.hasPaid)}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Confirmed Payment: </span> <span
+                  class="black--text">{{ confirmed( item.hasPaid ) }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile v-if='item.hasPaid'>
+          <v-list-tile
+            v-if="item.hasPaid">
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Mode of Payment: </span> <span class="black--text">{{item.modepay}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Mode of Payment: </span> <span
+                  class="black--text">{{ item.modepay }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Order Date: </span> <span class="black--text">{{$dateFilter(item.orddate)}}</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Order Date: </span> <span
+                  class="black--text">{{ $dateFilter(item.orddate) }}</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <v-list-tile-sub-title><span class="caption grey--text">Total Price: </span> <span class="black--text">Php {{item.itemstotal}}.00</span></v-list-tile-sub-title>
+              <v-list-tile-sub-title><span
+                class="caption grey--text">Total Price: </span> <span
+                  class="black--text">Php {{ item.itemstotal }}.00</span></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-group>
@@ -57,9 +80,9 @@ export default {
   methods: {
     confirmed(val){
       if(val){
-        return 'Yes'
+        return "Yes"
       }else{
-        return 'No'
+        return "No"
       }
     }
   }
