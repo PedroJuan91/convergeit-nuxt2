46ead3df0ef0a251b757302444e32b45b2fd6184
pedrojuan <peter.damondamon@davao.sti.edu>
2018-11-19 21:50:55 +0800

    config components/admin/agent/payment/full

--- components/admin/agent/payment/full.vue
+++ components/admin/agent/payment/full.vue
@@ -1,42 +1,59 @@
 <template>
   <v-flex>
-    <v-list-group v-for='(item, index) in items' :key='index'>
-      <v-list-tile slot="activator">
-        <v-list-tile-title>{{item.ordnameto}}</v-list-tile-title>
+    <v-list-group
+      v-for="(item, index) in items"
+      :key="index">
+      <v-list-tile
+        slot="activator">
+        <v-list-tile-title>{{ item.ordnameto }}</v-list-tile-title>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Order Physical Address: </span><span class="black--text">{{item.address}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Order Physical Address: </span><span
+              class="black--text">{{ item.address }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Order Email Address: </span><span class="black--text">{{item.ordsend}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Order Email Address: </span><span
+              class="black--text">{{ item.ordsend }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Payer name: </span><span class="black--text">{{item.payer_dat.payer_info.last_name}}, {{item.payer_dat.payer_info.first_name}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Payer name: </span><span
+              class="black--text">{{ item.payer_dat.payer_info.last_name }}, {{ item.payer_dat.payer_info.first_name }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Payer Email Address: </span><span class="black--text">{{item.payer_dat.payer_info.email}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Payer Email Address: </span><span
+              class="black--text">{{ item.payer_dat.payer_info.email }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Order date: </span><span class="black--text">{{$dateFilter(item.orddate)}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Order date: </span><span
+              class="black--text">{{ $dateFilter(item.orddate) }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Payment date: </span><span class="black--text">{{$dateFilter(item.paid_timesnap)}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Payment date: </span><span
+              class="black--text">{{ $dateFilter(item.paid_timesnap) }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
       <v-list-tile>
         <v-list-tile-content>
-          <v-list-tile-sub-title><span class="caption">Payment amount: </span><span class="black--text">Php {{item.paid_item.amount.total}}</span> </v-list-tile-sub-title>
+          <v-list-tile-sub-title><span
+            class="caption">Payment amount: </span><span
+              class="black--text">Php {{ item.paid_item.amount.total }}</span> </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list-group>
