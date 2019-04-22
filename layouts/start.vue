ca71bf4dd2b51938141861ebcd1edad33b5fa4ef
PedroJuan <peter.damondamon@davao.sti.edu>
2019-04-22 12:39:55 +0800

    remove header @ login

--- layouts/start.vue
+++ layouts/start.vue
@@ -1,35 +1,5 @@
 <template>
   <v-app dark >
-    <header>
-
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-      >
-        <v-list 
-          v-for="item in items" 
-          :key="item.id">
-          <v-list-tile
-            :to="item.to"
-            router
-          >
-            <v-list-tile-action>
-              <v-icon
-              >{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title
-              >{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <theHeader
-        :main-title="namedefault"
-        :btns="headeritems"
-        @toggle-drawer="drawer = !drawer"
-      />
-    </header>
     <v-content>
       <v-container>
         <nuxt />
