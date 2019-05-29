2d2b73a6a8f92d4a5734d59cdbf86d5db830a730
PedroJuan <peter.damondamon@davao.sti.edu>
2019-05-29 12:16:36 +0800

    finish load up data for orders

--- store/modules/report.js
+++ store/modules/report.js
@@ -5,10 +5,35 @@ export default {
     perClient: []
   },
   getters: {
-
+    filterdate(state){
+      return state.perClient.sort((dateA, dateB) => {
+        return
+      })
+    }
   },
-  mutations: {},
+  mutations: {
+    perClt(state, payload){
+      state.perClient = payload
+    }
+  },
   actions: {
+    getCltOrder({commit, dispatch, getters, rootGetters}, payload){
+      const agent = rootGetters.agentKey
+      async function getData(){
+        let loads = []
+        const rooter = await firebase.database().ref()
+        const data = await rooter.child(`orders/${agent}`).once('value', snap => {
+          const inside = snap.val()
+          for(let n in inside){
+            if(inside[n].cltid == payload){
+              loads.push({id: n, ...inside[n]})
+            }
+          }
+        })
+        console.log(loads)
+      }
+      return getData().catch(e => console.log('error on getCltOrder', e))
+    },
     convertpfd({commit, dispatch, getters, rootGetters}){
       return new Promise((resolve, reject) => {
         const agent = rootGetters.agentKey
